14e5e750131d9dd3cf55012f571dfdcd11e1e99f
ahao <ahao@uw.edu>
2022-12-16 10:44:07 -0800

    Header comments

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /*
  * Date: Dec. 16, 2022
- * Description
+ * The index.html page does not use any Javascript currently, but I am going to
+ * keep this file here in case I do decide to add some.
  */
 
 "use strict";
